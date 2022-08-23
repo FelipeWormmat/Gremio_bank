bf83890c660bf7ee680b44c8b3c9a4678e70e694
Felipe Wormmat <felipe.wormmat@gmail.com>
2022-08-23 20:47:06 -0300

    first commit

---
+++ src/Conta.java
@@ -0,0 +1,7 @@
+public class Conta {
+	
+	public int Numero;
+	public int Agencia;
+	public double Saldo;
+	public Cliente cliente;
+}
---
+++ src/GremioBank.java
@@ -0,0 +1,5 @@
+public class GremioBank {
+	public static void main(String[] args) {
+		
+	} 
+}
