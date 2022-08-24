21ffa66ea606362e618c4ddf1d4fcfbf126401bd
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2022-08-23 21:27:07 -0300

    Ajuste do projeto de POO

--- src/Banco.java
+++ src/Banco.java
@@ -1,3 +1,22 @@
-public class Banco {
+import java.util.List;
 
+public class Banco {
+    public String Nome;
+    public List<Conta> contas;
+
+    public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		this.Nome = nome;
+	}
+
+	public List<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(List<Conta> contas) {
+		this.contas = contas;
+	}
 }
--- src/GremioBank.java
+++ src/GremioBank.java
@@ -1,5 +1,6 @@
 public class GremioBank {
 	public static void main(String[] args) {
-		
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
 	} 
 }
