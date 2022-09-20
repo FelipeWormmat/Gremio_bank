2a3a4d3ac3c57daf969617dab9319fd6dd53f60b
Fernando Pimmel <pimmelfernando@gmail.com>
2022-09-20 01:27:29 -0300

    Remoção da classe extrato

--- src/GremioBank.java
+++ src/GremioBank.java
@@ -40,45 +40,26 @@ public class GremioBank {
 		System.out.println("============   DADOS DO BANCO   ===============");
 		System.out.println("===============================================");
 		System.out.printf("Banco: %s\n", banco.getNome());
-<<<<<<< HEAD
 		System.out.printf("Agência: %s\n\n", banco.getAgencia().getNumero());
-=======
-		System.out.printf("Agência: %s\n\n", agencia.getNumero());
->>>>>>> 52aad5ae255d28a851ec4aad7905eabfe3545cdb
 		
 		/*	PRINT DOS DADOS DO CLIENTE	*/
 		System.out.println("===============================================");
 		System.out.println("===========   DADOS DO CLIENTE   ==============");
 		System.out.println("===============================================");
 		System.out.printf("Nome: %s\n", cliente1.getNome());
-<<<<<<< HEAD
 		System.out.printf("CPF: %s\n", cliente1.getCpf().getCpf());
 		System.out.printf("Email: %s\n", cliente1.getEmail().getEmail());
 		System.out.printf("Celular: %s\n", cliente1.getCelular().getCelular());
 		System.out.printf("Endereço: %s, %s\n\n", cliente1.getEndereco().getCidade(), cliente1.getEndereco().getEstado());
-=======
-		System.out.printf("CPF: %s\n", cpf1.getCpf());
-		System.out.printf("Email: %s\n", email1.getEmail());
-		System.out.printf("Celular: %s\n", celular1.getCelular());
-		System.out.printf("Endereço: %s, %s\n\n", endereco1.getCidade(), endereco1.getEstado());
->>>>>>> 52aad5ae255d28a851ec4aad7905eabfe3545cdb
 		
 		/*	PRINT DOS DADOS DA CONTA	*/
 		System.out.println("===============================================");
 		System.out.println("============   DADOS DA CONTA   ===============");
 		System.out.println("===============================================");
-<<<<<<< HEAD
 		System.out.printf("Banco: %s\n", conta1.getBanco().getNome());
 		System.out.printf("Cliente: %s\n", conta1.getCliente().getNome());
 		System.out.printf("Pix: %s\n", conta1.getPix().getChave());
 		System.out.printf("Cartão: %s\n", conta1.getCartao().getFuncao().getFuncao());
 		System.out.printf("Saldo: %s\n\n", conta1.getSaldo().getSaldo());
-=======
-		System.out.printf("Banco: %s\n", banco.getNome());
-		System.out.printf("Cliente: %s\n", cliente1.getNome());
-		System.out.printf("Pix: %s\n", pix1.getChave());
-		System.out.printf("Cartão: %s\n", tipoCartao1.getFuncao());
-		System.out.printf("Saldo: R$ %s\n\n", saldo1.getSaldo());
->>>>>>> 52aad5ae255d28a851ec4aad7905eabfe3545cdb
 	} 
 }
